536f26d5ecb144ffda6e484f2822ef97e6848a33
Your Name <your@email.com>
2026-04-25 18:50:59 +0530

    force update

--- backend/server.js
+++ backend/server.js
@@ -166,3 +166,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running UPDATED");
+});
